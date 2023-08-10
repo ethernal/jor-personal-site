f7d36672c79d6c0dfc2c4b6b2f54e125e6bc8215
Sebastian Pieczyński <testament777@gmail.com>
2023-08-10 17:48:54 +0200

    feat: ✨ Dark Mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import {
-  Work_Sans,
-  Spline_Sans_Mono,
-} from 'next/font/google';
+import { Work_Sans, Spline_Sans_Mono } from 'next/font/google';
 import clsx from 'clsx';
+import { cookies } from 'next/headers';
 
 import { LIGHT_TOKENS, DARK_TOKENS } from '@/constants';
 
@@ -27,7 +25,12 @@ const monoFont = Spline_Sans_Mono({
 
 function RootLayout({ children }) {
 	// TODO: Dynamic theme depending on user preference
-	const theme = 'light';
+	const savedTheme = cookies().get('theme');
+	const theme = savedTheme?.value ?? 'light';
+
+	const toggleTheme = () => {
+		coockies((currentTheme) => (currentTheme === 'light' ? 'dark' : 'light'));
+	};
 
 	return (
 		<MotionPreferencesConfig>
@@ -38,7 +41,7 @@ function RootLayout({ children }) {
 				style={theme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}
 			>
 				<body>
-					<Header theme={theme} />
+					<Header initialTheme={theme} />
 					<main>{children}</main>
 					<Footer />
 				</body>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import clsx from 'clsx';
 import { Rss, Sun, Moon } from 'react-feather';
 
@@ -6,37 +7,49 @@ import Logo from '@/components/Logo';
 import VisuallyHidden from '@/components/VisuallyHidden';
 
 import styles from './Header.module.css';
+import Cookie from 'js-cookie';
+import { DARK_COLORS, LIGHT_COLORS } from '@/constants';
 
-function Header({ theme, className, ...delegated }) {
-  return (
-    <header
-      className={clsx(styles.wrapper, className)}
-      {...delegated}
-    >
-      <Logo />
+function Header({ initialTheme, className, ...delegated }) {
+	const [theme, setTheme] = useState(initialTheme);
 
-      <div className={styles.actions}>
-        <button className={styles.action}>
-          <Rss
-            size="1.5rem"
-            style={{
-              // Optical alignment
-              transform: 'translate(2px, -2px)',
-            }}
-          />
-          <VisuallyHidden>
-            View RSS feed
-          </VisuallyHidden>
-        </button>
-        <button className={styles.action}>
-          <Sun size="1.5rem" />
-          <VisuallyHidden>
-            Toggle dark / light mode
-          </VisuallyHidden>
-        </button>
-      </div>
-    </header>
-  );
+	const handleThemeChange = () => {
+		const nextTheme = theme === 'light' ? 'dark' : 'light';
+		setTheme(nextTheme);
+		Cookie.set('theme', nextTheme, { expires: 380 });
+
+		const root = document.documentElement;
+		root.setAttribute('data-color-theme', nextTheme);
+
+		const colors = nextTheme === 'light' ? LIGHT_COLORS : DARK_COLORS;
+
+		Object.entries(colors).forEach(([key, value]) => {
+			root.style.setProperty(key, value);
+		});
+	};
+
+	return (
+		<header className={clsx(styles.wrapper, className)} {...delegated}>
+			<Logo />
+
+			<div className={styles.actions}>
+				<button className={styles.action}>
+					<Rss
+						size="1.5rem"
+						style={{
+							// Optical alignment
+							transform: 'translate(2px, -2px)',
+						}}
+					/>
+					<VisuallyHidden>View RSS feed</VisuallyHidden>
+				</button>
+				<button className={styles.action} onClick={handleThemeChange}>
+					{theme === 'light' ? <Sun size="1.5rem" /> : <Moon size="1.5rem" />}
+					<VisuallyHidden>Toggle dark / light mode</VisuallyHidden>
+				</button>
+			</div>
+		</header>
+	);
 }
 
 export default Header;
