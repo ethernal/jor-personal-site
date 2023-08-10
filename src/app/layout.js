75d549f8dd339d450df18424a2e807354604fd4d
Sebastian Pieczyński <testament777@gmail.com>
2023-08-10 13:42:32 +0200

    feat: 💫 animate equation visualization

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,38 +10,41 @@ import { LIGHT_TOKENS, DARK_TOKENS } from '@/constants';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import './styles.css';
+import MotionPreferencesConfig from '@/components/MotionPreferencesConfig/MotionPreferencesConfig';
 
 const mainFont = Work_Sans({
-  subsets: ['latin'],
-  display: 'fallback',
-  weight: 'variable',
-  variable: '--font-family',
+	subsets: ['latin'],
+	display: 'fallback',
+	weight: 'variable',
+	variable: '--font-family',
 });
 const monoFont = Spline_Sans_Mono({
-  subsets: ['latin'],
-  display: 'fallback',
-  weight: 'variable',
-  variable: '--font-family-mono',
+	subsets: ['latin'],
+	display: 'fallback',
+	weight: 'variable',
+	variable: '--font-family-mono',
 });
 
 function RootLayout({ children }) {
-  // TODO: Dynamic theme depending on user preference
-  const theme = 'light';
+	// TODO: Dynamic theme depending on user preference
+	const theme = 'light';
 
-  return (
-    <html
-      lang="en"
-      className={clsx(mainFont.variable, monoFont.variable)}
-      data-color-theme={theme}
-      style={theme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}
-    >
-      <body>
-        <Header theme={theme} />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
-  );
+	return (
+		<MotionPreferencesConfig>
+			<html
+				lang="en"
+				className={clsx(mainFont.variable, monoFont.variable)}
+				data-color-theme={theme}
+				style={theme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}
+			>
+				<body>
+					<Header theme={theme} />
+					<main>{children}</main>
+					<Footer />
+				</body>
+			</html>
+		</MotionPreferencesConfig>
+	);
 }
 
 export default RootLayout;
